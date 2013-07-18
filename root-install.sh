80fda4ffd2dab2e78c04d7e4bcbf439e0313a958
Daniel Cousens <daniel210x@gmail.com>
2013-07-18 17:35:04 +1000

    Base commit

---
+++ root-install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+hostname="arch"
+
+kbl="us"
+locale="en_AU.UTF-8 UTF-8"
+timezone="/usr/share/zoneinfo/Australia/Melbourne"
+
+
+function create_nonroot_user() {
+	echo "Enter non-root username:"
+	read username
+	useradd -m -g users -s /bin/bash $username
+	passwd $username
+}
+
+function do_locale() {
+	echo "$hostname" > /etc/hostname
+	ln -s $timezone /etc/localtime
+
+	echo "KEYMAP=$kbl" > /etc/vconsole.conf
+	echo "FONT=lat9w-16" >> /etc/vconsole.conf
+	echo "FONT_MAP=8859-1_to_uni" >> /etc/vconsole.conf
+
+	echo $locale > /etc/locale.gen
+	locale-gen
+	locale > /etc/locale.conf
+}
+
+function do_iptables() {
+	cp iptables.rules /etc/iptables/iptables.rules
+	cp ip6tables.rules /etc/iptables/ip6tables.rules
+
+	chmod 700 /etc/iptables/
+}
+
+function do_services() {
+	systemctl enable dhcpcd.service
+	systemctl enable iptables.service
+	systemctl enable ip6tables.service
+
+	chmod 700 /etc/systemd/
+}
+
+function update_mirrorlist() {
+	curl "https://www.archlinux.org/mirrorlist/?country=AU" | sed 's/#Server/Server/g' > /etc/pacman.d/mirrorlist
+}
+
+do_locale
+update_mirrorlist
+do_iptables
+do_services
+create_nonroot_user
