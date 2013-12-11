581defd405bbf74577cfbaf0c7b46a8d17463012
Daniel Cousens <daniel210x@gmail.com>
2013-12-11 13:29:51 +1100

    Moves all extra packages to extra.txt

--- wm-install.sh
+++ wm-install.sh
@@ -8,8 +8,7 @@ if [ -z $(grep "archlinuxfr" /etc/pacman.conf) ]; then
 	pacman -Sy yaourt
 fi
 
-packages="dmenu mesa xorg-server xorg-server-utils xorg-xinit"
-extra=$(cat extra.txt)
+packages=$(cat extra.txt)
 
 echo "Select [initial] graphics driver:"
 select yn in "Nvidia" "Ati" "Intel" "None"; do
@@ -21,6 +20,6 @@ select yn in "Nvidia" "Ati" "Intel" "None"; do
 	esac
 done
 
-yaourt -Sy $packages $extra $gpudriver
+yaourt -Sy $packages $gpudriver
 
 #makepkg -fi --skipinteg --noconfirm
